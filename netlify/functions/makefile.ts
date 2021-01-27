5c19bc84f16d7bd0b671b384d4f971cb862b0d5d
Adam Burgess <adamburgess@users.noreply.github.com>
2021-01-27 22:13:56 +1100

    update makefile

--- netlify/functions/makefile.ts
+++ netlify/functions/makefile.ts
@@ -63,7 +63,8 @@ function imageToTarget(image: string, repos: string[], dockers: string[], packag
     const targets = [repoTargets, dockerTargets, packageTargets, inputTargets];
     return `image-${imageFile}: ${targets.filter(t => t.length !== 0).join(' ')}
 \t@echo [Image] ${image}
-\t@cd ../dockerfiles && docker build -t aburgess/${image} -f ${imageFile}.Dockerfile . > ../builder/image-${imageFile}
+\tcd ../dockerfiles && docker build -t aburgess/${image} -f ${imageFile}.Dockerfile .
+\t@touch image-${imageFile}
 \tdocker push aburgess/${image}
 
 `
