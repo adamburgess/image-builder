9b65ef89332dbbe490bac5296365214076414764
Adam Burgess <adamburgess@users.noreply.github.com>
2021-01-27 18:54:53 +1100

    packages

---
+++ netlify-functions/alpine.js
@@ -0,0 +1,29 @@
+const AlpineApk = require('alpine-apk');
+
+const alpineApk = new AlpineApk();
+
+let updatedAlpine = false;
+
+async function updatePackages() {
+    if (!updatedAlpine) {
+        await alpineApk.update();
+        updatedAlpine = true;
+    }
+}
+
+async function getDependenciesForPackages(packages) {
+    let tree = alpineApk.getDependencyTree(...packages);
+    return tree.split(',').filter(x => x).sort();
+}
+
+exports.handler = async function (event, context) {
+    const packages = event.queryStringParameters.packages.split(' ');
+    const dependencies = await getDependenciesForPackages(packages);
+    return {
+        statusCode: 200,
+        headers: {
+            'Content-Type': 'text/plain'
+        },
+        body: JSON.stringify(dependencies)
+    };
+}
