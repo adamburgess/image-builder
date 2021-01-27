e5dd63b421c7a8c2733952d76447504f7d246a3a
Adam Burgess <adamburgess@users.noreply.github.com>
2021-01-27 18:24:32 +1100

    test functions

---
+++ netlify-functions/test.js
@@ -0,0 +1,12 @@
+import yosay from 'yosay'
+
+exports.handler = async function (event, context) {
+    const text = yosay('hi ' + event.path);
+    return {
+        statusCode: 200,
+        headers: {
+            'Content-Type': 'text/plain'
+        },
+        body: text
+    };
+}
